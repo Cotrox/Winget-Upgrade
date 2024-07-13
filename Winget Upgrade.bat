b2ca9c6d330d973c00827a7eddc6278e7e0673af
Giuseppe Antonino Cotroneo <62651507+Cotrox@users.noreply.github.com>
2024-07-13 09:24:34 +0200

    - Fix Multi Ambient Execution Problem

--- Winget Upgrade.bat
+++ Winget Upgrade.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Creazione dello script VBS
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "cmd.exe", "/c winget upgrade --accept-package-agreements --all", "", "runas", 1 >> "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "cmd.exe", "/k winget upgrade --accept-package-agreements --all && pause", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
 :: Esecuzione dello script VBS
 "%temp%\getadmin.vbs"
